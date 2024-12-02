0038e4a935553ac84767bb207a2e38be48ba803c
cmdtorch <emil4154515@gmail.com>
2024-12-02 08:18:50 +0100

    change user sign in data and getter

--- app/services/social.py
+++ app/services/social.py
@@ -3,24 +3,24 @@ import jwt
 from google.auth.transport import requests as google_requests
 from google.oauth2 import id_token
 
-from app.core.schemas.auth import SocUserInfo
+from app.core.schemas.auth import UserSocialData
 
 
 class SocAuth:
     @classmethod
-    def google(cls, token: str) -> SocUserInfo | None:
+    def google(cls, token: str) -> UserSocialData | None:
         request = google_requests.Request()
         info = id_token.verify_oauth2_token(token, request, clock_skew_in_seconds=300)
-        return SocUserInfo(
+        return UserSocialData(
             email=info["email"],
             full_name=f"{info['given_name']} {info['given_name']}",
         )
 
     @classmethod
-    def apple(cls, token: str) -> SocUserInfo | None:
+    def apple(cls, token: str) -> UserSocialData | None:
         try:
             payload = jwt.decode(token, options={"verify_signature": False})
-            return SocUserInfo(
+            return UserSocialData(
                 email=payload.get("email"),
                 full_name=f"{payload.get("givenName")} {payload.get("familyName")}",
             )
@@ -28,12 +28,12 @@ class SocAuth:
             return None
 
     @classmethod
-    def facebook(cls, token: str) -> SocUserInfo | None:
+    def facebook(cls, token: str) -> UserSocialData | None:
         response = httpx.get(
             f"https://graph.facebook.com/me?access_token={token}&fields=id,first_name,last_name,email,picture",
         )
         response_json = response.json()
-        return SocUserInfo(
+        return UserSocialData(
             email=response_json.get("email"),
             full_name=f"{response_json.get('first_name')} "
             f"{response_json.get('last_name')}",
--- app/services/user.py
+++ app/services/user.py
@@ -1,7 +1,9 @@
+from typing import Any
+
 from app.core.db.transactional import Propagation, Transactional
 from app.core.exceptions.auth import WrongTimezoneException
 from app.core.models import User
-from app.core.schemas.auth import UserAuthData
+from app.core.schemas.auth import SignIn
 from app.core.utils import is_valid_timezone
 from app.repository.user import UserRepository, user_repository
 
@@ -10,16 +12,16 @@ from .base import BaseService
 
 class UserService(BaseService[User, UserRepository]):
     @Transactional(Propagation.REQUIRED)
-    async def create_user(self, user_data: UserAuthData) -> User:
-        user = User(**user_data.model_dump())
+    async def create_user(self, user_data: Any) -> User:
+        user = User(**user_data)
         return await self.repository.save(user)
 
-    async def update_on_login(self, user: User, auth_data: UserAuthData) -> User:
+    async def update_on_login(self, user: User, auth_data: SignIn) -> User:
         if not is_valid_timezone(auth_data.timezone):
             raise WrongTimezoneException
 
-        return await user_repository.update(
-            updates=auth_data.model_dump(exclude={"soc_type"}),
+        return await self.repository.update(
+            updates=auth_data.model_dump(exclude={"email", "password"}),
             instance=user,
         )
 
